5f87240b32bdfb5fd5b00709136ae4f3f2b4a9ff
schrader <schrader-git-win@v9k.de>
2018-03-19 23:26:24 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='unicorn_hat_sim',
+    version='1.0.0',
+    description='A simple Unicorn Hat simulator',
+    long_description=long_description,
+    url='https://github.com/jayniz/unicorn-hat-sim',
+    author='jayniz',
+    author_email='jannis@gmail.com',
+    py_modules=['unicorn_hat_sim.py'],
+)
